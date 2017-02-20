67a4f60fce94df9044fd70579bc28d67ed3b749f
Kaluchii <mr.kaluchii@mail.ru>
2017-02-20 20:09:06 +0600

    Main page adapt verified

--- resources/views/front/footer.blade.php
+++ resources/views/front/footer.blade.php
@@ -1,12 +1,15 @@
 @section('footer')
     <footer class="footer">
         <div class="bottom-row">
-            <p class="bottom-text">{{$static->copyright_field}}</p>
-            <p class="bottom-text">Комплекс в социальных сетях:
-                <span class="soc-links"><a href="{{$static->vk_field}}" rel="nofollow" target="_blank" class="link">ВКонтакте</a><a
-                            href="{{$static->inst_field}}" rel="nofollow" target="_blank" class="link">Инстаграм</a></span>
+            <p class="bottom-text left">{{$static->copyright_field}}</p>
+            <p class="bottom-text center">Комплекс в социальных сетях:<br>
+                <span class="soc-links">
+                    <a href="{{$static->vk_field}}" rel="nofollow" target="_blank" class="link">ВКонтакте</a>
+                    <a href="{{$static->inst_field}}" rel="nofollow" target="_blank" class="link">Инстаграм</a>
+                    <a href="{{$static->fb_field}}" rel="nofollow" target="_blank" class="link">Фейсбук</a>
+                </span>
             </p>
-            <p class="bottom-text">Сделано в <a href="http://interpro.kz" rel="nofollow" target="_blank" class="link">ИнтерПро</a>
+            <p class="bottom-text right">Сделано в<br><a href="http://interpro.kz" rel="nofollow" target="_blank" class="link">ИнтерПро</a>
             </p>
         </div>
     </footer>
